44110eb26a393465cc360b9a977f7362f3be2712
s0derlund <70884414+s0derlund@users.noreply.github.com>
2020-10-23 17:01:13 +0200

    Add files via upload

---
+++ sketch_servoTester.ino
@@ -0,0 +1,35 @@
+#define SENSOR_KARTONG 8
+#define SENSOR_METALL 7
+#define SENSOR_PLAST 6
+
+void setup() 
+{
+  // put your setup code here, to run once:
+  pinMode(SENSOR_KARTONG, INPUT);
+  pinMode(SENSOR_METALL, INPUT);
+  pinMode(SENSOR_PLAST, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  if(digitalRead(SENSOR_KARTONG) == HIGH)
+  {
+    Serial.println("K");
+  }
+  else if(digitalRead(SENSOR_METALL) == HIGH)
+  {
+    Serial.println("M");
+  }
+  else if(digitalRead(SENSOR_PLAST) == HIGH)
+  {
+    Serial.println("P");
+  }
+  else
+  {
+    Serial.println(0);
+  }
+    
+  delay(1000);
+}
